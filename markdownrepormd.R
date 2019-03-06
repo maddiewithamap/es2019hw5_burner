9dda624fdfdc27563ff68b88eb51dfc534e04235
MADELINE BROWN <MBROWN40@UCMERCED.EDU>
2019-03-05 18:59:03 -0800

    committing 19 code

---
+++ markdownrepormd.R
@@ -0,0 +1,19 @@
+---
+title: "Rmarkdownforgit"
+author: "Madeline Brown"
+date: "March 5, 2019"
+output: 
+  html_document: 
+    keep_md: yes
+---
+  #' Here's some prose in a very special comment. Let's summarize the built-in
+  #' dataset `VADeaths`.
+  ## here is a regular code comment, that will remain as such
+  summary(VADeaths)
+
+#' Here's some more prose. I can use usual markdown syntax to make things
+#' **bold** or *italics*. Let's use an example from the `dotchart()` help to
+#' make a Cleveland dot plot from the `VADeaths` data. I even bother to name
+#' this chunk, so the resulting PNG has a decent name.
+#+ dotchart
+dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
